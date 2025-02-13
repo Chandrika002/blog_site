8b562fe138a07719e504795950884a58609626d3
Chandrika002 <chandrika.csecu@gmail.com>
2025-02-13 15:09:49 +0600

    fifth commit

--- posts/models.py
+++ posts/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length = 60)
+    title= models.CharField(max_length=60)
     content = models.TextField()
     publish_date= models.DateField()
 
-class Comment(models.Model):
-    text = models.TextField(100)
+class comment(models.Model):
+    text = models.CharField(max_length=100)
+     
