df31d0e0813a775bc899f36a91cbbbd396fcc5f7
Chandrika002 <chandrika.csecu@gmail.com>
2025-02-13 15:08:12 +0600

    fourth commit

--- posts/admin.py
+++ posts/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from posts.models import Post, Comment
+from posts.models import Post,comment
+class PostAdmin (admin.ModelAdmin):
+    list_display=["id","title","publish_date"]
 # Register your models here.
-class PostAdmin(admin.ModelAdmin):
-    list_display = ["id", "title", "publish_date"]
-admin.site.register(Post, PostAdmin)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ["id", "text"]
 
+admin.site.register(Post,PostAdmin)
+
+class CommnetAdmin (admin.ModelAdmin):
+    list_display=["id","text"]
+admin.site.register(comment,CommnetAdmin)
