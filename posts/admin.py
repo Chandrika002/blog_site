a395a59da0e428609fa92a4a3af4b437e8d36d03
Chandrika002 <chandrika.csecu@gmail.com>
2025-02-13 13:51:19 +0600

    Initial commit

---
+++ posts/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from posts.models import Post, Comment
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "publish_date"]
+admin.site.register(Post, PostAdmin)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["id", "text"]
+
